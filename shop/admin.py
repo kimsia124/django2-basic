65ff95add67f9ec29d72b3070b1b81913ce5ccd2
sia <kimsia124@gmail.com>
2019-05-01 15:29:40 +0900

    [ADD] shop item add is publish

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+# shop/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Item
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'short_desc','price', 'is_publish']
+    list_display_links = ['pk', 'name', 'short_desc']
+    list_filter = ['is_publish']
+    search_fields = ['name']
+
+    def short_desc(self,item):
+        return item.desc[:20]
