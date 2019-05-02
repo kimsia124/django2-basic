34f4f27c27cae0a72d888c04fb25bd61e6dcc0ed
sia <kimsia124@gmail.com>
2019-05-02 20:24:09 +0900

    [ADD] blog author_name field add

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+# blog/models.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['pk']
--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
 # blog/modles.py
 
 from django.db import models
+from django.conf import settings
 
 class Post(models.Model):
+    author_name = models.CharField(max_length=20, default="user")
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
@@ -11,3 +13,4 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     message = models.TextField()
+ 
